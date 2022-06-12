49595237c435a65289bb0fe0d6e5036cbc838e67
Davit Khudaverdyan <davkhuda@gmail.com>
2022-06-12 08:56:02 -0700

    install and use Morgan for logging, only if in dev environment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Load environment variables
 dotenv.config({
@@ -9,8 +10,13 @@ dotenv.config({
 // Load routes
 const bootcamps = require('./routes/bootcamps');
 
-
 const app = express();
+
+// app.use(logger); <- fun exercise no longer will use
+
+if (process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
