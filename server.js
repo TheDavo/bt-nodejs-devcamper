28e2311ffe0b5804c9c811fb0f238062b4a0d4ce
Davit Khudaverdyan <davkhuda@gmail.com>
2022-06-12 10:20:50 -0700

    add connection to DB and handle promise rejections

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({
   path: './config/config.env'
 });
+
+// Connect to DB
+connectDB();
 
 // Load routes
 const bootcamps = require('./routes/bootcamps');
@@ -24,6 +28,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, 
+const server = app.listen(PORT, 
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
